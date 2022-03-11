9443899a9f586f8e231efe009fb0ae66ba33ceb8
pawelplutauek20 <pawelpluta58@gmail.com>
2022-03-11 21:24:09 +0100

    responsive width

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   AppShell,
   Header,
@@ -7,6 +8,10 @@ import {
   ScrollArea,
   Table,
   Title,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
 } from "@mantine/core";
 
 const elements = [
@@ -27,64 +32,83 @@ const elements2 = [
 ];
 
 const App = () => {
+  const [opened, setOpened] = React.useState(false);
+  const theme = useMantineTheme();
+
   const rows = elements.map((element, index) => (
     <tr key={index}>
       <td>{element.name}</td>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
-        <td>
-          <NumberInput hideControls />
-        </td>
-      ))}
+      {index !== 3 &&
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
+          <td>
+            <NumberInput hideControls />
+          </td>
+        ))}
     </tr>
   ));
 
   const rows2 = elements2.map((element, index) => (
     <tr key={index}>
       <td>{element}</td>
-      {[1, 2, 3, 4, 5, 6].map(() => (
-        <td>
-          <NumberInput hideControls />
-        </td>
-      ))}
+      {index !== 6 &&
+        [1, 2, 3, 4, 5, 6].map(() => (
+          <td>
+            <NumberInput hideControls />
+          </td>
+        ))}
     </tr>
   ));
 
   return (
-    <MantineProvider theme={{ colorScheme: "dark" }}>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: "dark" }}
+    >
       <AppShell
+        navbarOffsetBreakpoint="sm"
         padding="md"
+        fixed
         navbar={
-          <Navbar width={{ base: 300 }} height={"100%"} p="xs">
-            {/* Navbar content */}
+          <Navbar
+            p="md"
+            hiddenBreakpoint="sm"
+            hidden={!opened}
+            width={{ sm: 300, lg: 400 }}
+          >
+            <Text>Application navbar</Text>
           </Navbar>
         }
         header={
-          <Header height={60} p="xs">
-            {/* Header content */}
+          <Header height={70} p="md">
+            <div
+              style={{ display: "flex", alignItems: "center", height: "100%" }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+
+              <Text>Application header</Text>
+            </div>
           </Header>
         }
-        styles={(theme) => ({
+        styles={{
           main: {
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
             paddingRight: 0,
           },
           body: {
             height: `calc(100% - 60px)`,
           },
-        })}
+        }}
       >
         <ScrollArea style={{ height: "100%", paddingRight: 16 }}>
-          <Title
-            sx={(theme) => ({
-              color: theme.colors.gray[0],
-            })}
-            order={1}
-          >
-            GHP
-          </Title>
+          <Title order={1}>GHP</Title>
           <Table sx={{ marginTop: 16, marginBottom: 16 }}>
             <thead>
               <tr>
@@ -96,14 +120,7 @@ const App = () => {
             </thead>
             <tbody>{rows}</tbody>
           </Table>
-          <Title
-            sx={(theme) => ({
-              color: theme.colors.gray[0],
-            })}
-            order={1}
-          >
-            MRP
-          </Title>
+          <Title order={1}>MRP</Title>
           <Table sx={{ marginTop: 16, marginBottom: 16 }}>
             <thead>
               <tr>
